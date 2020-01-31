e9c63cefc20a9794b0681d198f779c8a505abe2b
Jinwoo Kim <kidjinwoo@me.com>
2020-01-30 16:17:24 -0800

    Upload

---
+++ ArrayOperatorsExtension.swift
@@ -0,0 +1,61 @@
+import Foundation
+
+// Extension for Array where Element: Equatable
+extension Array where Element: Equatable {
+    func intersection(_ arr: Array<Element>) -> Array<Element> {
+        var result: [Element] = []
+        var arr = arr
+        
+        for a in self {
+            for b in arr {
+                if a == b {
+                    if let idx = arr.firstIndex(of: a) {
+                        arr.remove(at: idx)
+                        result.append(a)
+                    }
+                }
+            }
+        }
+        
+        return result
+    }
+
+    func union(_ arr: Array<Element>) -> Array<Element> {
+        var result = self + arr
+        for a in self.intersection(arr) {
+            if let idx = result.firstIndex(of: a) {
+                result.remove(at: idx)
+            }
+        }
+        return result
+    }
+    
+    func symmetricDifference(_ arr: Array<Element>) -> Array<Element> {
+        var result: [Element] = self.union(arr)
+        for a in self.intersection(arr) {
+            if let idx = result.firstIndex(of: a) {
+                result.remove(at: idx)
+            }
+        }
+        return result
+    }
+    
+    func subtracting(_ arr: Array<Element>) -> Array<Element> {
+        var result = self
+        for a in self.intersection(arr) {
+            if let idx = result.firstIndex(of: a) {
+                result.remove(at: idx)
+            }
+        }
+        return result
+    }
+}
+
+// Test
+var a = [1, 2, 3, 4, 5, 5, 5]
+var b = [4, 5, 5, 3, 8, 9]
+
+print(a.intersection(b)) // [3, 4, 5, 5]
+print(a.symmetricDifference(b)) // [1, 2, 5, 8, 9]
+print(a.union(b).sorted()) // [1, 2, 3, 4, 5, 5, 5, 8, 9]
+print(a.subtracting(b)) // [1, 2, 5]
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Array_Operators_Swift
+
+![1](https://live.staticflickr.com/65535/49466511222_f3736c08b9_o.png)
+
+Image from [Swift](https://docs.swift.org/swift-book/LanguageGuide/CollectionTypes.html)
+
 Extension of Fundamental Set Operations for Array, without converting Array to Set.
+
+## Examples
+
+```swift
+var a = [1, 2, 3, 4, 5, 5, 5]
+var b = [4, 5, 5, 3, 8, 9]
+
+print(a.intersection(b)) // [3, 4, 5, 5]
+print(a.symmetricDifference(b)) // [1, 2, 5, 8, 9]
+print(a.union(b).sorted()) // [1, 2, 3, 4, 5, 5, 5, 8, 9]
+print(a.subtracting(b)) // [1, 2, 5]
+```
